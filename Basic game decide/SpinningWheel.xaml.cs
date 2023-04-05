20612501d8267bf2a45b6d4ca6f4d84145e5d626
JustAGoofBall <justinlitcom777@gmail.com>
2023-04-05 12:48:27 +0200

    more windows

---
+++ Basic game decide/SpinningWheel.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Basic_game_decide
+{
+    /// <summary>
+    /// Interaction logic for SpinningWheel.xaml
+    /// </summary>
+    public partial class SpinningWheel : Window
+    {
+        public SpinningWheel()
+        {
+            InitializeComponent();
+        }
+    }
+}
