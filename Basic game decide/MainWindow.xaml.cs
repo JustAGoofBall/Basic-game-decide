4c65ff2bd4084c3b65ed66b9c3b1ed33c2b375f9
JustAGoofBall <justinlitcom777@gmail.com>
2023-04-05 12:46:28 +0200

    Add project files.

---
+++ Basic game decide/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Basic_game_decide
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Roshambo_Click(object sender, RoutedEventArgs e)
+        {
+            // new windows for diffrent games
+            Roshambo roshambo = new Roshambo();
+
+            roshambo.Show();
+            this.Hide();
+        }
+
+        private void SpinningWheel_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Spinning Wheel");
+            Roshambo roshambo = new Roshambo();
+
+            roshambo.Show();
+            this.Hide();
+        }
+
+        private void Pacman_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("PacMan");
+            Roshambo roshambo = new Roshambo();
+
+            roshambo.Show();
+            this.Hide();
+        }
+
+        private void BlackJack_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Black Jack");
+            Roshambo roshambo = new Roshambo();
+
+            roshambo.Show();
+            this.Hide();
+        }
+    }
+}
