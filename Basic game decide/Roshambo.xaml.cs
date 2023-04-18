3a36d867fec837b03b84898ca6e88cfd1bb7fc3e
JustAGoofBall <justinlitcom777@gmail.com>
2023-04-18 15:44:32 +0200

    more changes

--- Basic game decide/Roshambo.xaml.cs
+++ Basic game decide/Roshambo.xaml.cs
@@ -19,9 +19,56 @@ namespace Basic_game_decide
     /// </summary>
     public partial class Roshambo : Window
     {
+        private string[] choices = { "rock", "paper", "scissors" };
+
         public Roshambo()
         {
             InitializeComponent();
         }
+
+        private void play(string playerMove)
+        {
+            Random random = new Random();
+            int computerChoice = random.Next(0, 3);
+            string computerMove = choices[computerChoice];
+
+            string result = "";
+
+            if (playerMove == computerMove)
+            {
+                result = "It's a tie!";
+            }
+            else if (playerMove == "rock" && computerMove == "scissors"
+                || playerMove == "paper" && computerMove == "rock"
+                || playerMove == "scissors" && computerMove == "paper")
+            {
+                result = "You won!";
+            }
+            else
+            {
+                result = "You lost!";
+            }
+
+            MessageBox.Show("You chose " + playerMove + ".\nComputer chose " + computerMove + ".\n" + result);
+        }
+
+        private void rock_click(object sender, RoutedEventArgs e)
+        {
+            play("rock");
+            Image img = MyImage;
+            img.Source = new BitmapImage(new Uri(@"C:\Data\Own project\Basic game decide\Basic game decide\Images\Paper.png"));
+        }
+
+
+        private void paper_click(object sender, RoutedEventArgs e)
+        {
+            play("paper");
+        }
+
+        private void scissor_click(object sender, RoutedEventArgs e)
+        {
+            play("scissors");
+        }
+
     }
 }
